2e59207ab1d75d12c05cfcbc2743e21ba4581014
Paweł Hanzlik <63288357+PawelHanzlik@users.noreply.github.com>
2021-06-08 19:34:17 +0200

    Add files via upload

---
+++ PH_WeatherApp/PH_WeatherApp/WeatherFetcher.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherFetcher.swift
+//  PH_WeatherApp
+//
+//  Created by AppleLab on 08/06/2021.
+//
+
+import Foundation
+import Combine
+
+class WeatherFetcher{
+    
+    func forecast(forId woeId: String) -> AnyPublisher<WeatherResponse, Error>{
+        let url: URL = URL(string: "https://www.metaweather.com/api/location/\(woeId)/")!
+        return URLSession.shared.dataTaskPublisher(for: url).map{$0.data}.decode(type: WeatherResponse.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+}
