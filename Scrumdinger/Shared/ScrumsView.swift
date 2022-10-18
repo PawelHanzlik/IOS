9a09dfe111075019a2821b9a952195fbf88b6081
Paweł Hanzlik <63288357+PawelHanzlik@users.noreply.github.com>
2022-10-18 12:59:50 +0200

    Add files via upload

--- Scrumdinger/Shared/ScrumsView.swift
+++ Scrumdinger/Shared/ScrumsView.swift
@@ -11,17 +11,29 @@ struct ScrumsView: View {
     let scrums: [DailyScrum]
     var body: some View {
         List{
-            ForEach(scrums, id: \.title){
+            ForEach(scrums){
                 scrum in
-                Card_View(scrum: scrum)
-                    .listRowBackground(scrum.theme.mainColor)
+                NavigationLink(destination: DetailView(scrum: scrum)){
+                    Card_View(scrum: scrum)
+                        .listRowBackground(scrum.theme.mainColor)
+                }
+                .listRowBackground(scrum.theme.mainColor)
             }
+        }
+        .navigationTitle("Daily Scrums")
+        .toolbar{
+            Button(action: {}){
+                Image(systemName: "plus")
+            }
+            .accessibilityLabel("New Scrum")
         }
     }
 }
 
 struct ScrumsView_Previews: PreviewProvider {
     static var previews: some View {
-        ScrumsView(scrums: DailyScrum.sampleData)
+        NavigationView{
+            ScrumsView(scrums: DailyScrum.sampleData)
+        }
     }
 }
